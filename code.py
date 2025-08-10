3990d3d17b18387a6203dccefa5529b5fc8a3051
Imesh Ranaweera <imeshchamara57@gmail.com>
2025-08-10 16:30:20 +0000

    initial

--- code.py
+++ code.py
@@ -3,5 +3,5 @@ import numpy as np
 
 np.random.seed(0)
 #d = scn.load("debug")
-#d = scn.load(casp_version=12, casp_thinning=100)
-d = scn.load(casp_version=12, casp_thinning=100, scn_dir='/orange/alberto.perezant/imesh.ranaweera/test_softwares/noesy_chainNet/sidechainnet_data/sidechainnet_casp12_100.pkl')
+d = scn.load(casp_version=12, casp_thinning=100)
+#d = scn.load(casp_version=12, casp_thinning=100, scn_dir='/orange/alberto.perezant/imesh.ranaweera/test_softwares/noesy_chainNet/sidechainnet_data/sidechainnet_casp12_100.pkl')
