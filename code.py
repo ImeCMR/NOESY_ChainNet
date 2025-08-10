f06e1e77d7be19a8b573be8337e1f3aef4221005
Imesh Ranaweera <imeshchamara57@gmail.com>
2025-08-10 15:32:42 +0000

    Initial

---
+++ code.py
@@ -0,0 +1,7 @@
+import sidechainnet as scn
+import numpy as np
+
+np.random.seed(0)
+#d = scn.load("debug")
+#d = scn.load(casp_version=12, casp_thinning=100)
+d = scn.load(casp_version=12, casp_thinning=100, scn_dir='/orange/alberto.perezant/imesh.ranaweera/test_softwares/noesy_chainNet/sidechainnet_data/sidechainnet_casp12_100.pkl')
